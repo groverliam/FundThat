dbb790d97161c0597723cb813b36c590f1d8fd76
groverliam <groverliam@gmail.com>
2019-04-24 09:20:55 -0400

    Update accountView.php

--- accountView.php
+++ accountView.php
@@ -159,15 +159,15 @@
     			die("Connection failed: " . $conn->connect_error);
 			} 
 
-			//$result = mysql_query("Select * from Deposits where Tax_ID = (Select Tax_ID from Customers Where Email = '$Email')");
+			/*$result = mysql_query("Select * from Deposits where Tax_ID = (Select Tax_ID from Customers Where Email = '$Email')");
 			//$result = mysql_query("Select * from Deposits where Tax_ID = (Select Tax_ID from Customers Where Email = '$Email') and Account_Number = $Account_Number");
-			//if (!$result) {
-			//    echo 'Could not run query: ' . mysql_error();
+			if (!$result) {
+			    echo 'Could not run query: ' . mysql_error();
 			    //echo 'This could be from from using incorrect Account Number';
 			    //$conn->close();	
-			//    exit;
-			//}
-			$row = mysql_fetch_row($result);
+			    exit;
+			}
+			$row = mysql_fetch_row($result);*/
 
 			//echo $row[Account_Number]; // 42
 			// sql to insert data to table*/
